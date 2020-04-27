a83ed11e097a1818031eecb6d5aba0fec9b6d5ff
vhasousa <vitorhugo_as@hotmail.com>
2020-04-27 17:21:21 -0300

    book_crud

---
+++ backend/src/app/controllers/BookController.js
@@ -0,0 +1,39 @@
+import Book from '../models/Book';
+
+class BookController {
+  async store(req, res) {
+    const book = await Book.create(req.body);
+
+    return res.json(book);
+  }
+
+  async index(req, res) {
+    const book = await Book.findAll();
+
+    return res.json(book);
+  }
+
+  async show(req, res) {
+    const book = await Book.findByPk(req.params.id);
+
+    return res.json(book);
+  }
+
+  async update(req, res) {
+    const bookUpdate = await Book.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    return res.json(bookUpdate);
+  }
+
+  async delete(req, res) {
+    await Book.destroy({ where: { id: req.params.id } });
+
+    return res.send();
+  }
+}
+
+export default new BookController();
